baebc8f6ac319838aec24e58d07a01a671ca6bd0
brendt <brent.roose@gmail.com>
2026-06-16 10:39:37 +0200

    chore: release v3.11.7

--- src/Kernel.php
+++ src/Kernel.php
@@ -8,7 +8,7 @@ use Tempest\Container\Container;
 
 interface Kernel
 {
-    public const string VERSION = '3.11.6';
+    public const string VERSION = '3.11.7';
 
     public string $root { get; }
 
